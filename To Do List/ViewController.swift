00aeafa8295a492a7732ee8b5d70cdd034b625b7
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-22 23:30:28 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Amal Agrawal on 9/22/19.
+//  Copyright © 2019 Amal Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
